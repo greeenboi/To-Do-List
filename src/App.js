5c6d318e763914e48e46a0ead9ef473588f97141
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-08-04 12:49:27 +0530

    2nd change to background

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ function App() {
   return (
     <main>
       <section>
-        <form className='flex flex-col w-1/2 shadow bg-slate-500'>
-          <input type="text" placeholder="tasks" className='h-10'/>
-          <button type="submit" >submit</button>
+        <h1>My Tasks of the day</h1>
+        <form className='flex flex-col w-1/2 shadow  justify-evenly gap-2'>
+          <input type="text" placeholder="tasks" className='h-10 border p-2 w-full text-xl rounded-sm'/>
+          <button type="submit" className='border p-4 bg-stone-600 border-l-sky-500 opacity-80 rounded-lg'>submit</button>
         </form>
+
+        <ul>
+          <li></li>
+        </ul>
 
       </section>
       
